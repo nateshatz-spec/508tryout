185f50271c2d61113149e5697683a641d0030604
nshatz7 <nateshatz08@icloud.com>
2026-04-28 21:25:28 -0400

    Initial Commit

---
+++ 508 tryout/_08_tryoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  _08_tryoutApp.swift
+//  508 tryout
+//
+//  Created by Nathaniel on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct _08_tryoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
